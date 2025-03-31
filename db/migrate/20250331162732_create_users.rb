f85cadc9c75fe20ccd1ec56eeb182d04c74b7ad6
Avneet Kaur <avneetkaur5@academic.rrc.ca>
2025-03-31 11:40:17 -0500

    initial commit

---
+++ db/migrate/20250331162732_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.text :address
+      t.string :phone_number
+      t.string :role
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
